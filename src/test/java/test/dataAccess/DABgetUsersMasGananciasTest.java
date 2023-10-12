b679b8d2599adee5057b122700310d5a80582814
Juan Navarlaz Urtubia <jnavarlaz002@ikasle.ehu.eus>
2023-10-12 12:10:42 +0200

    fix inf

--- src/test/java/test/dataAccess/DABgetUsersMasGananciasTest.java
+++ src/test/java/test/dataAccess/DABgetUsersMasGananciasTest.java
@@ -19,6 +19,7 @@ public class DABgetUsersMasGananciasTest {
     public void setUp() {
         dataAccess = new DataAccess(true); // Inicializa con el modo de inicialización
         dataAccess.open(true);
+        
         //dataAccess.initializeDB(); // Inicializa la base de datos con datos de prueba
     }
     @After
@@ -34,22 +35,22 @@ public class DABgetUsersMasGananciasTest {
 
     @Test
     public void testGetUsersMasGananciasWithUsersAndTransactions() {
-        User user1 = dataAccess.getUser("x1");
-        User user2 = dataAccess.getUser("x2");
+        User user1 = dataAccess.getUser("Paco");
+        User user2 = dataAccess.getUser("juan");
         
         // Agregar transacciones a los usuarios
-        dataAccess.addDinero("x1", 100.0f, false);
-        dataAccess.anadirApuesta("x1", 1, 50.0f, "Local");
-        dataAccess.addDinero("x2", 200.0f, false);
-        dataAccess.anadirApuesta("x2", 1, 150.0f, "Empate");
-        dataAccess.anadirApuesta("x2", 2, 50.0f, "1-2 goles");
+        dataAccess.addDinero("Paco", 100.0f, false);
+        dataAccess.anadirApuesta("Paco", 1, 50.0f, "Local");
+        dataAccess.addDinero("Juan", 200.0f, false);
+        dataAccess.anadirApuesta("Juan", 1, 150.0f, "Empate");
+        dataAccess.anadirApuesta("Juan", 2, 50.0f, "1-2 goles");
         
         Vector<Object> result = dataAccess.getUsersMasGanancias();
 
         // Verificar que el resultado contiene los usuarios y sus ganancias
-        assertTrue(result.contains("x1"));
+        assertTrue(result.contains("Paco"));
         assertTrue(result.contains(50.0f)); // (100 - 50)
-        assertTrue(result.contains("x2"));
+        assertTrue(result.contains("Juan"));
         assertTrue(result.contains(100.0f)); // (200 - 150 + 50)
     }
 }
--- src/test/java/test/dataAccess/DAWuserExistTest.java
+++ src/test/java/test/dataAccess/DAWuserExistTest.java
@@ -3,14 +3,28 @@ package test.dataAccess;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import dataAccess.DataAccess;
+import domain.User;
 
 public class DAWuserExistTest {
+	DataAccess dataAccess;
+	@Before
+    public void setUp() {
+        DataAccess dataAccess = new DataAccess(true); // Inicializa con el modo de inicialización
+        dataAccess.open(true);
+        //dataAccess.initializeDB(); // Inicializa la base de datos con datos de prueba
+    }
+    @After
+    public void close() {
+    	dataAccess.close();
+    }
 	@Test
     public void testUserExistWithExistingUser() {
-        DataAccess dataAccess = new DataAccess();
+        User user1 = dataAccess.getUser("carlos");
         // Supongamos que "Carlos" ya existe en la base de datos
         boolean userExists = dataAccess.userExist("Carlos");
         assertTrue(userExists);
