24395e6db77ba372f84c2c0be4483b84ec368765
Juan Navarlaz Urtubia <jnavarlaz002@ikasle.ehu.eus>
2023-11-11 12:23:09 +0100

    main funcional

--- src/main/java/gui/MainForIterator.java
+++ src/main/java/gui/MainForIterator.java
@@ -7,44 +7,45 @@ import java.util.Calendar;
 import java.util.Date;
 
 import businessLogic.BLFacade;
+import businessLogic.BLFacadeImplementation;
 import businessLogic.ExtendedIterator;
 import configuration.BLFactory;
+import configuration.ConfigXML;
 import configuration.UtilDate;
+import dataAccess.DataAccess;
 import domain.Event;
+import exceptions.EventAlreadyExist;
 
 public class MainForIterator {
 	public static void main(String[] args)	{
-	//	obtener el	objeto Facade	local
-	int isLocal =	1;
-	BLFacade	blFacade =	new BLFactory().getBusinessLogicFactory(isLocal);
-	//SimpleDateFormat	sdf =	new SimpleDateFormat("dd/MM/yyyy");
-	Calendar today = Calendar.getInstance();
-	int month=today.get(Calendar.MONTH);
-	month+=1;
-	int year=today.get(Calendar.YEAR);
-	if (month==12) { month=0; year+=1;}
-	Date	date = UtilDate.newDate(year,month,17);
-	try {
-		System.out.println("llega aqui");
-		//date = sdf.parse("17/12/2023"); //	17	del	mes	que	viene
-		ExtendedIterator<Event>	i =	blFacade.getEventsIterator(date);
-		Event	e;
-		System.out.println("_____________________");
-		System.out.println("RECORRIDO	HACIA	ATRÁS");
-		i.goLast();	//	Hacia atrás
-		while (i.hasPrevious())	{
-			e =	i.previous();
-			System.out.println(e.toString());
+		// obtener el objeto Facade local
+		ConfigXML c=ConfigXML.getInstance();
+		BLFacade blFacade;
+		DataAccess da = new DataAccess(c.getDataBaseOpenMode().equals("initialize"));
+		blFacade = new BLFacadeImplementation(da);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date date;
+		try {
+			date = sdf.parse("17/12/2020"); // 17 del mes que viene
+			ExtendedIterator<Event> i = blFacade.getEventsIterator(date);
+			Event e;
+			System.out.println("_____________________");
+			System.out.println("RECORRIDO HACIA ATRÁS");
+			i.goLast(); // Hacia atrás
+			while (i.hasPrevious()) {
+				e = i.previous();
+				System.out.println(e.toString());
+			}
+			System.out.println();
+			System.out.println("_____________________");
+			System.out.println("RECORRIDO HACIA ADELANTE");
+			i.goFirst(); // Hacia adelante
+			while (i.hasNext()) {
+				e = i.next();
+				System.out.println(e.toString());
+			}
+		} catch (ParseException e) {
+			System.out.println("Problems with date?? " + "17/12/2020");
 		}
-		System.out.println();
-		System.out.println("_____________________");
-		System.out.println("RECORRIDO	HACIA	ADELANTE");
-		i.goFirst();	//	Hacia adelante
-		while (i.hasNext())	{
-			e =	i.next();
-			System.out.println(e.toString());
-		}
-	} catch (Exception	e1)	{
-		System.out.println("Problems	with	date??	" +	date);
-	}
+		
 }}
